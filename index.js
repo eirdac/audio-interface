78003672645b244d66baced841753eb38d9b981d
Adrienne Cheng <adrienne.cheng97@gmail.com>
2020-12-20 19:22:24 -0500

    added click img to play audio

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
+// adding a click function where user clicks image to start and stop music
 
+document.getElementById("clickImageMusic").onclick = function() {
+    var audio = document.getElementById("audio");
+    if (audio.paused) audio.play();
+    else audio.pause();
+};
 
